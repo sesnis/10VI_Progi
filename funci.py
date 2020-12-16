b2a4bcf028aaee50d9e5db03ec15825f370e540f
sesnis <71374072+sesnis@users.noreply.github.com>
2020-12-16 11:08:09 +0200

    new stuff

---
+++ funci.py
@@ -0,0 +1,4 @@
+#muusu funkcijas
+#triistura perimetrs
+def tr_per(x1,x2,x3)
+    return x1+x2+x3
--- galva.py
+++ galva.py
@@ -3,13 +3,16 @@ from flask import Flask
 from flask import request
 from flask import url_for
 from flask import render_template
+from funci import *
 
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("sveikaPasaule.html")
-
+    aa = float(request.args.get('a',default='0.',type=str))
+    bb = float(request.args.get('b',default='0.',type=str))
+    cc = float(request.args.get('c',default='0.',type=str))
+    rez = "perimetrs="+str(funci.tr_per(aa,bb,cc))
 
 @app.route('/tests')
 def health():
